9edfad5488289ec5bb5a2c39f884e09eaba2db9f
Thiago Garcia <thiagoncg1@gmail.com>
2024-08-15 22:01:51 -0300

    torre

---
+++ Scripts/torre.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void hanoi(int n, char origem, char auxiliar, char destino) {
+    if (n == 1) {
+        cout << "Mover disco 1 de " << origem << " para " << destino << endl;
+        return;
+    }
+    hanoi(n - 1, origem, destino, auxiliar);
+    cout << "Mover disco " << n << " de " << origem << " para " << destino << endl;
+    hanoi(n - 1, auxiliar, origem, destino);
+}
+
+int main() {
+    int n;
+    cout << "Digite o número de discos: ";
+    cin >> n;
+
+    hanoi(n, 'A', 'B', 'C');
+
+    return 0;
+}
